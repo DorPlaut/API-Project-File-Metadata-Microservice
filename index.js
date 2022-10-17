1f6289d6ef61fa6c48d41e936cd3fe7a08e05759
dorplaut <dorplaut@gmail.com>
2022-10-18 01:43:39 +0300

    initial upload

---
+++ index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var cors = require('cors');
+require('dotenv').config();
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+// my selution
+// handle post file data
+const handlePostFile = async (req, res) => {
+  const { originalname, mimetype, size } = req.file;
+  res.status(202).json({
+    name: originalname,
+    type: mimetype,
+    size: size,
+  });
+};
+
+// post request and handle errors
+app.post('/api/fileanalyse', upload.single('upfile'), async (req, res) => {
+  try {
+    await handlePostFile(req, res);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port);
+});
